4bb43b37480e74057fffd4c50fef9803e11c2a6d
Devansh Bhagania <117916160+Devansh-Bhagania@users.noreply.github.com>
2023-11-13 08:36:38 +0530

    helmet changed

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Helmet = (props) => {
-  document.title = "Jannat Travels | Cabs | Tours |" + props.title;
+  document.title = "Jannat Travels | Cabs | Tours | Taxi |"
   return <div className="w-100">{props.children}</div>;
 };
 
